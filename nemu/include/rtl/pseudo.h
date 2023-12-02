6fe3cdcd697bddd3358b79fd18c0c02170238386
leoshy <le0shy@outlook.com>
2023-12-01 17:18:43 -0800

    2a: start working on dummy

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -7,35 +7,62 @@
 
 /* RTL pseudo instructions */
 
-static inline def_rtl(li, rtlreg_t* dest, const rtlreg_t imm) {
+static inline def_rtl(li, rtlreg_t *dest, const rtlreg_t imm)
+{
   rtl_addi(s, dest, rz, imm);
 }
 
-static inline def_rtl(mv, rtlreg_t* dest, const rtlreg_t *src1) {
+static inline def_rtl(mv, rtlreg_t *dest, const rtlreg_t *src1)
+{
   rtl_addi(s, dest, src1, 0);
 }
 
-static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t* src1) {
+static inline def_rtl(not, rtlreg_t *dest, const rtlreg_t *src1)
+{
   // dest <- ~src1
   TODO();
 }
 
-static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
+static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t *src1)
+{
   // dest <- -src1
   TODO();
 }
 
-static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline def_rtl(sext, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  //TODO();
+  Assert(width > sizeof(word_t) * 4, "Immediate number is invalid");
+  /* Obtain the sign bit */
+  word_t fetch_bit = 1;
+  for (int i = 1; i < width; i =i + 1)
+  {
+    fetch_bit <<= 1;
+  }
+  word_t sign_bit = fetch_bit & *src1;
+
+  /* Extend the sign bit */
+  *dest = *src1;
+  if (sign_bit != 0)
+  {
+    word_t signext_count = sizeof(word_t) * 4 - width;
+    for (int i = 0; i < signext_count; i = i+ 1)
+    {
+      sign_bit <<= 1;
+      *dest |= sign_bit;
+    }
+  }
 }
 
-static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline def_rtl(zext, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
   TODO();
 }
 
-static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline def_rtl(msb, rtlreg_t *dest, const rtlreg_t *src1, int width)
+{
   // dest <- src1[width * 8 - 1]
   TODO();
 }
--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,17 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+
+def_EHelper(addi){
+  rtl_addi(s, ddest, id_src1->preg, id_src2->simm );
+}
+
+def_EHelper(auipc){
+  rtl_addi(s, ddest, &s->pc, id_src1->imm );
+}
+
+def_EHelper(jal){
+  rtl_addi(s, ddest, &s->snpc, 0);
+  rtl_sext(s, s0, &id_src1->imm, 21);
+  s->dnpc = s->pc + *s0;
+}
