139a8042380d603c48cfff7e0d9551b7670981bf
leoshy <le0shy@outlook.com>
2023-12-24 00:24:14 -0800

    2.1b: add B type instr.

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -3,7 +3,9 @@
 
 #define INSTR_LIST(f) f(lui) f(lb) f(lh) f(lw) f(lbu) f(lhu) \
 f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) \
-f(srai) f(sw) f(sb) f(sh) f(addi) f(add) f(sub) f(sll) f(srl) f(sra) f(slt) \
+f(srai) f(sw) f(sb) f(sh) f(addi) \
+f(add) f(sub) f(sll) f(srl) f(sra) f(slt) \
+f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) \
 f(sltu) f(xor) f(or) f(and) f(auipc) f(jal) f(jalr) f(inv) f(nemu_trap)
 
 def_all_EXEC_ID();
